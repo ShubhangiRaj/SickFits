f6073dd6e879f971702cf4ca83361c831aa1aa2f
Shubhangi Raj <shubhi.raj13@gmail.com>
2019-05-07 12:34:52 +0530

    initial commit

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'next/link';
+
+class Home extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Hi</h1>
+            </div>
+        )
+    }
+}
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'next/link';
+
+class Sell extends React.Component{
+    render(){
+        return(
+            <div> 
+                <h1>Sell</h1>
+            </div>
+        )
+    }
+}
+
+export default Sell;
